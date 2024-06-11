60bd56c4eaed05294bf9eaedd238cb4b8a9fb123
Imamhassan <imamhsn195@gmail.com>
2024-06-11 17:09:03 +0400

    Data Type literal

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 4.45;
+        char firstInitial = 'I';
+        char lastInitial = 'N';
+        System.out.println("Age: " + studentAge);
+        System.out.println("GPA: " + studentGPA);
+        System.out.println("First Initial : " + firstInitial);
+        System.out.println("Last Initial: " + lastInitial);
+    }
+}
