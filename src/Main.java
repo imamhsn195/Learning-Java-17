6fb4ec7c042434e4d555a826b75dd38bc3b63b69
Imamhassan <imamhsn195@gmail.com>
2024-06-12 12:42:50 +0400

    Building Strong Foundation

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 4.45;
-        char firstInitial = 'I';
-        char lastInitial = 'N';
         String firstName = "Imam";
         String lastName = "Hasan";
+        char firstInitial = firstName.charAt(0);
+        char lastInitial = lastName.charAt(0);
         System.out.println("Age: " + studentAge);
         System.out.println("GPA: " + studentGPA);
         System.out.println("First Initial : " + firstInitial);
         System.out.println("Last Initial: " + lastInitial);
         System.out.println("First Name: " + firstName);
         System.out.println("Last Name: " + lastName);
+        System.out.println(firstName + " " + lastName + " has of GPA " + studentGPA);
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(firstName + " " + lastName + " now has of GPA " + studentGPA);
     }
 }
