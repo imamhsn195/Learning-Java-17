9ad79c68fd0b7a0468d654f94c19f6875f372e1d
Imamhassan <imamhsn195@gmail.com>
2024-06-19 10:45:35 +0400

    Build-in functions

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void calculateTotalMealPrice(Double listedPrice, Double tipRate, Double taxRate){
+    public static double calculateTotalMealPrice(Double listedPrice, Double tipRate, Double taxRate){
         Double tips = listedPrice * tipRate;
         Double taxes = listedPrice * taxRate;
         Double totalMealPrice = listedPrice + tips + taxes;
-        System.out.println("Your total meal price is " + totalMealPrice);
+        return totalMealPrice;
     }
     public static void main(String[] args) {
-        calculateTotalMealPrice(15.0,0.3,0.25);
+        double groupMealTotal = calculateTotalMealPrice(100.0,0.2,0.25);
+        double perMemberMealTotal = groupMealTotal/5;
+        System.out.println("Per member cost: " + perMemberMealTotal);
     }
 }
