62fafc07396bcb4910137dfff2f1d2accb4ed836
Imamhassan <imamhsn195@gmail.com>
2024-06-20 10:18:59 +0400

    Finding area method

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
         Triangle triangleB = new Triangle(3, 2.598, 3,3,3);
+
+        double triangleAArea = triangleA.findArea();
+        System.out.println("Area of A is " + triangleAArea);
+
+        double triangleBArea = triangleB.findArea();
+        System.out.println("Area of B is " + triangleBArea);
     }
 }
