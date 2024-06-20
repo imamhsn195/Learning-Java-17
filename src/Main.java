2d933f1e0c6863d86008fb47b6399b6bc773330c
Imamhassan <imamhsn195@gmail.com>
2024-06-20 10:11:11 +0400

    Creating a java class

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int studentAge = 15;
-        double studentGPA = 4.45;
-        String firstName = "Imam";
-        String lastName = "Hasan";
-        char firstInitial = firstName.charAt(0);
-        char lastInitial = lastName.charAt(0);
-        System.out.println("Age: " + studentAge);
-        System.out.println("GPA: " + studentGPA);
-        System.out.println("First Initial : " + firstInitial);
-        System.out.println("Last Initial: " + lastInitial);
-        System.out.println("First Name: " + firstName);
-        System.out.println("Last Name: " + lastName);
-        System.out.println(firstName + " " + lastName + " has of GPA " + studentGPA);
-
-        Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
-
-        System.out.println(firstName + " " + lastName + " now has of GPA " + studentGPA);
+        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+        Triangle triangleB = new Triangle(3, 2.598, 3,3,3);
     }
 }
